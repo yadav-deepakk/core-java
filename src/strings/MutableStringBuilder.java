e67591f8a1392cd91a9c56bcc298c1fe03f15d9c
yadav-deepakk <deepakyadav53882@gmail.com>
2023-12-08 00:42:10 +0530

    removed bin files

--- src/strings/MutableStringBuilder.java
+++ src/strings/MutableStringBuilder.java
@@ -6,13 +6,15 @@ public class MutableStringBuilder {
 		// can be initialized with capacity or char sequence. 
 		StringBuffer sbuff = new StringBuffer(); 
 		StringBuilder sbldr = new StringBuilder(); 
-		System.out.println(sbuff.capacity()); // 16
-		System.out.println(sbldr.capacity()); // 16
+		System.out.println(sbuff.capacity()); // capacity - 16
+		System.out.println(sbldr.capacity()); // capacity - 16
 
 		StringBuffer sbuff2 = new StringBuffer(120); 
 		StringBuilder sbldr2 = new StringBuilder(150); 
-		System.out.println(sbuff2.capacity()); // 120
-		System.out.println(sbldr2.capacity()); // 150
+		System.out.println(sbuff2.capacity()); // capacity - 120
+		System.out.println(sbldr2.capacity()); // capacity - 150
+		
+		
 
 	}
 
