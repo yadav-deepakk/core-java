1ed376d6b2e145176a5efc5249b40c95800c768a
yadav-deepakk <deepakyadav53882@gmail.com>
2023-12-24 10:57:42 +0530

    Collection Framework:- ArrayList

--- src/collections/ArrayListDemo.java
+++ src/collections/ArrayListDemo.java
@@ -5,23 +5,52 @@ import java.util.ArrayList;
 
 public class ArrayListDemo {
 
-	public static void main(String[] args) {
-		List<Integer> array_list = new ArrayList<>(10);
-		System.out.println(array_list);
-		
-		for (int i=1;i<=10;i++)	array_list.add(i);					// add
-		System.out.println(array_list);
-		
-		array_list.remove(7);										// remove
-		
-		for (int j=0;j<array_list.size();j++)	
-			System.out.print(array_list.get(j) + " ");				// get
-		
-		System.out.println("\nAccessing elements using Enhanced for loop");		
+	public static void arrayListMethod1() {
+		List<Integer> arrayList = new ArrayList<>(10);
+		System.out.println(arrayList);
 
-		for(int num: array_list)
+		for (int i = 1; i <= 10; i++)
+			arrayList.add(i); // add
+		System.out.println(arrayList);
+
+		arrayList.remove(7); // remove
+
+		for (int j = 0; j < arrayList.size(); j++)
+			System.out.print(arrayList.get(j) + " "); // get
+
+		System.out.println("\nAccessing elements using Enhanced for loop");
+
+		for (int num : arrayList)
 			System.out.print(num + " ");
-		
+	}
+
+	public static void arrayListMethod2() {
+		ArrayList evenNums = new ArrayList();
+		evenNums.add(2);
+		evenNums.add(4);
+		evenNums.add(6);
+		evenNums.add(8);
+
+		ArrayList arrList = new ArrayList();
+		arrList.add(11);
+		arrList.add(19);
+		arrList.add(0, 4);
+		arrList.add("string1");
+		arrList.add('c');
+		arrList.addAll(0, evenNums);
+
+		arrList.remove(0);
+
+		// loop over elements of an arrayList
+		for (int i = 0; i < arrList.size(); i++) {
+			System.out.print(arrList.get(i) + " ");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		arrayListMethod1(); 
+		arrayListMethod2();
 	}
 
 }
