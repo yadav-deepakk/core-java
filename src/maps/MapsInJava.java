9302236dd111f64458d18a875e35adb34ea76506
yadav-deepakk <deepakyadav53882@gmail.com>
2023-12-27 23:31:58 +0530

    Maps: HashMap

--- src/maps/MapsInJava.java
+++ src/maps/MapsInJava.java
@@ -1,9 +1,24 @@
 package maps;
 
+import java.util.*;
+
 public class MapsInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// HashMap -> may or may not maintains order of insertion.
+		HashMap map = new HashMap();
+
+		map.put(3, "Parth Singh");
+		map.put(4, "Nitesh Singh");
+		map.put(5, "Pravin Kumar");
+		map.put(2, "Dara Singh"); // will be changed.
+		map.put(null, null);
+		map.put(1, "Deepak Kumar");
+		map.put(2, "Tara Singh"); // will update previous value
+
+//		System.out.println(map);
+		map.forEach((key, value) -> System.out.println("Roll No: " + key + ", Name: " + value));
 
 	}
 
