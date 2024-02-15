06742e7765c74e2309629648e2d88ff9bf2f9033
yadav-deepakk <deepakyadav53882@gmail.com>
2024-02-15 07:33:15 +0530

    Graph searching - BFS Algorithm

--- src/graph/GraphAlgorithms.java
+++ src/graph/GraphAlgorithms.java
@@ -2,12 +2,12 @@ package graph;
 
 import java.util.*;
 
-public class GraphDFS {
+public class GraphAlgorithms {
 	private int V;
 	private LinkedList<Integer> adjList[];
 
 	@SuppressWarnings("unchecked")
-	public GraphDFS(int v) {
+	public GraphAlgorithms(int v) {
 		this.V = v;
 		this.adjList = new LinkedList[this.V];
 		for (int i = 0; i < this.V; ++i) {
@@ -34,12 +34,27 @@ public class GraphDFS {
 			int vertex = iter.next();
 			if (!visited[vertex])
 				this.dfsSearch(vertex, visited);
+		}
+	}
 
+	public void bfsSearch(int v) {
+		boolean[] visited = new boolean[this.V];
+		Queue<Integer> que = new LinkedList<Integer>();
+		que.add(v);
+		while (!que.isEmpty()) {
+			int vtx = que.poll();
+			if (visited[vtx])
+				continue;
+			visited[vtx] = true;
+			System.out.print(vtx + " ");
+			Iterator<Integer> itr = this.adjList[vtx].iterator();
+			while (itr.hasNext())
+				que.add(itr.next());
 		}
 	}
 
 	public static void main(String[] args) {
-		GraphDFS graph = new GraphDFS(5);
+		GraphAlgorithms graph = new GraphAlgorithms(5);
 //		int[][] edges = { { 0, 1 }, { 0, 2 }, { 1, 2 }, { 2, 0 }, { 3, 2 }, { 2, 3 } };
 		int[][] edges = { { 0, 2 }, { 0, 1 }, { 0, 3 }, { 1, 0 }, { 1, 3 }, { 2, 0 }, { 3, 0 }, { 3, 1 }, { 3, 4 },
 				{ 4, 3 } };
@@ -49,15 +64,24 @@ public class GraphDFS {
 			System.out.println("Edge: " + edge[0] + " --> " + edge[1] + " added " + (res ? "success" : "unsuccess"));
 		}
 
-		// DFS SEARCHING
 		int vtxCount = graph.getVertexCount();
+
+		// DFS SEARCHING
+		System.out.println("--------- DFS ----------");
 		for (int i = 0; i < vtxCount; i++) {
 			System.out.println("DFS Search at vertex " + i + ": ");
 			graph.dfsSearch(i, new boolean[vtxCount]);
 			System.out.println();
 		}
-		
-		
+
+		// BFS Searching
+		System.out.println();
+		System.out.println("--------- BFS -----------");
+		for (int i = 0; i < vtxCount; i++) {
+			System.out.println("BFS Search at vertex " + i + ": ");
+			graph.bfsSearch(i);
+			System.out.println();
+		}
 
 	}
 
